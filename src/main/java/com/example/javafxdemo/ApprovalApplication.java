8649665a6e63f61a84668b2b0a15c7d4180e68e3
natngu18 <natngu18@gmail.com>
2023-11-14 16:00:41 -0500

    fixed path

--- src/main/java/com/example/javafxdemo/ApprovalApplication.java
+++ src/main/java/com/example/javafxdemo/ApprovalApplication.java
@@ -5,13 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class ApprovalApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ApprovalApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("C:/Users/Nathan/Desktop/as/src/main/resources/com/example/javafxdemo/hello-view.fxml").toURI().toURL());
 
         // This is the Request Business Object passed to Approval Screen, so that the form fields will be pre-populated.
         GeneaologyRequestApproval passedRequest = new GeneaologyRequestApproval(123, new Citizen(1,
