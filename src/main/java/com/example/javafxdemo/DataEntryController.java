0b3d0a61683bed8c4ced98d3e6619cd7640500c6
areddy20 <aditialpoorreddy@gmail.com>
2023-11-17 09:08:00 -0500

    Implemented review screens and edited data-entry files

--- src/main/java/com/example/javafxdemo/DataEntryController.java
+++ src/main/java/com/example/javafxdemo/DataEntryController.java
@@ -17,8 +17,15 @@ public class DataEntryController {
     // Example Form Field
     // Made possible by this line in data-entry.fxml: <TextField fx:id="exampleTextField" promptText="This is example text field" />
     // "fx:id" must match this variable
-    @FXML
-    private TextField exampleTextField;
+
+    @FXML private TextField citizenNameField;
+    @FXML private TextField citizenIDField;
+    @FXML private TextField immigrantNameField;
+    @FXML private TextField immigrantIDField;
+    @FXML private TextField formIDField;
+    @FXML private TextField formDescriptionField;
+
+
 
     // empty constructor is required
     public DataEntryController() { }
@@ -29,11 +36,11 @@ public class DataEntryController {
     // Function to demonstrate how to read inputted text into the example text field when button is pressed.
     // Made possible by this line in data-entry.fxml: <Button text="Print example text field to terminal" onAction="#printExampleTextField" />
     // "onAction" must match this function name
-    public void printExampleTextField() {
+    //public void printExampleTextField() {
         // read inputted text
-        String value = exampleTextField.getText();
-        System.out.println(value);
-    }
+      //  String value = exampleTextField.getText();
+       // System.out.println(value);
+    //}
 
     // Called when button to switch to review screen is pressed.
     // May need to do additional things to pass data entered in data entry screen to the review screen.
@@ -46,6 +53,14 @@ public class DataEntryController {
         // 1. Calling a function defined in ReviewController:
         //        // Get the controller instance
         //        ReviewController reviewController = loader.getController();
+        ReviewController reviewController = loader.getController();
+        reviewController.setCitizenName(citizenNameField.getText());
+        reviewController.setCitizenID(citizenIDField.getText());
+        reviewController.setImmigrantID(immigrantIDField.getText());
+        reviewController.setImmigrantName(immigrantNameField.getText());
+        reviewController.setFormID(formIDField.getText());
+        reviewController.setFormDescription(formDescriptionField.getText());
+
         //
         //        // Pass data to the second controller by calling function defined in ReviewController
         //        reviewController.setImmigrantId("example");
