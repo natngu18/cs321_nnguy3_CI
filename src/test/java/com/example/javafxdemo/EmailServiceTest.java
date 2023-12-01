b487d8026d9a9fad655a08d34d3b4c070cba358f
natngu18 <natngu18@gmail.com>
2023-12-01 16:44:21 -0500

    removed tests

--- src/test/java/com/example/javafxdemo/EmailServiceTest.java
+++ src/test/java/com/example/javafxdemo/EmailServiceTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailServiceTest {
-
-
     // these emails should be valid
     @Test
     public void testValidEmails() {
